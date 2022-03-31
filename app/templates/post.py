b60aeea1e0e47a739bbe4afc7992ac7383972d8b
anand4211 <anand.singh4211@gmail.com>
2022-03-31 16:22:27 +0530

    movefiles

--- app/templates/post.py
+++
@@ -1,9 +0,0 @@
-class Movie(db.Model):
-    __tablename__ = 'movies'  # creating a table name
-    __table_args__ = (
-        CheckConstraint('Date < Current_Date',)
-    id = db.Column(db.Integer, primary_key=True)  # this is the primary key
-    title = db.Column(db.String(80), unique=True)
-    year = db.Column(db.Date, nullable=False)
-    genre = db.Column(db.String(80), nullable=False)
-
